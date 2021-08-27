f4fc3cefe2c84daf3e5b1e2571ed44c152296624
Richard Farukh <richardjohnson.farukh@sky.uk>
2021-08-27 14:06:59 +0100

    feat(true): script to create an alias for git-util

---
+++ create_alias.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASH_PROFILE=~/.bash_profile
+BASHRC=~/.bashrc
+ZSHRC=~/.zshrc
+DIRNAME="$( dirname "${BASH_SOURCE[0]}" &> /dev/null && pwd )"
+
+function add_alias {
+   echo "alias acp=$DIRNAME/script.rb" >> $1
+   exit 0;
+}
+
+if [[ -f $ZSHRC ]]; then 
+   add_alias $ZSHRC;
+elif [[ -f $BASH_PROFILE ]]; then
+   add_alias $BASH_PROFILE;
+elif [[ -f $BASHRC ]]; then
+    add_alias $BASHRC;
+else 
+   echo "No candidates for a script found - exiting script";
+fi
