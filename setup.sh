4c4ec376c618a4f2a0a396ea9b3186e938a6711e
Richard Farukh <richardjohnson.farukh@sky.uk>
2022-03-01 16:54:15 +0000

    feat: update setup.sh script

--- setup.sh
+++ setup.sh
@@ -8,24 +8,27 @@ DIRNAME="$( dirname "${BASH_SOURCE[0]}" &> /dev/null && pwd )"
 function add_alias {
    printf "\nalias acp=$DIRNAME/src/script.rb\n" >> $1
    echo "export GEM_HOME=\"$HOME/.gem\"" >> $1
-   echo  "---" >> "$DIRNAME/src/config/co-authors.yml"
    source $1
    gem install bundler && bundle
-
-   exit 0;
 }
 
-function app_setup {
+function app_file_setup {
    cp $DIRNAME/src/config/config_template.yml $DIRNAME/src/config/config.yml
    mkdir $DIRNAME/.cache
+   echo  "---" > "$DIRNAME/src/config/co-authors.yml"
 }
+
+app_file_setup
 
 if [[ -f $ZSHRC ]]; then 
    add_alias $ZSHRC;
 elif [[ -f $BASH_PROFILE ]]; then
    add_alias $BASH_PROFILE;
 elif [[ -f $BASHRC ]]; then
-    add_alias $BASHRC;
+   add_alias $BASHRC;
 else 
-   echo "No candidates for a script found - exiting script";
+   echo "No candidates for a script found - exiting";
+   exit 1
 fi
+
+echo "Installation complete! Use 'acp -h' for help with the command"
